697b369ff94d4811567766ee911cab262489bc28
Mostafa Kamal <radionishi@gmail.com>
2018-10-13 05:12:03 +0600

    underscore alternate by vowel

---
+++ ruby_code/change_underscore_w_vowel.rb
@@ -0,0 +1,28 @@
+# @code4mk kaaml_ie_t => 4 uniq combination (_=> uniq previous vowel)
+your_string =  gets.chomp()
+b = []
+b = your_string.split(//)
+count = 0
+# loop store_string
+b.each_with_index {|val, index|
+  if val == "_"
+    previous_datas = 0
+    previous_v = 0
+    store_vowel = []
+
+    while previous_datas < index
+      check_position =  b[previous_datas]
+      is_vowel = /[aeiou]/.match(check_position)
+
+      if is_vowel.class == MatchData
+        store_vowel[previous_v] =  is_vowel.to_s
+        previous_v = previous_v + 1
+      end
+      previous_datas = previous_datas + 1
+    end
+    for i in  store_vowel.uniq
+      count = count + 1
+    end
+  end
+}
+puts "#{your_string} =>  #{count} uniq combination"
