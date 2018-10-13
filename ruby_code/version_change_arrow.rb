567a8cea1e46457a5ccac2c374fad1fb3339de61
Mostafa Kamal <radionishi@gmail.com>
2018-10-13 06:17:20 +0600

    version change (method change)

---
+++ ruby_code/version_change_arrow.rb
@@ -0,0 +1,29 @@
+=begin
+@author: @code4mk
+======================================================
+        input                        output
+======================================================
+int a;                     || int a;
+a => 5; // this => arrow   || a . 5; // this => arrow
+b => 7: // 2nd => arrow    || b . 7: // 2nd => arrow
+======================================================
+=end
+
+users = "int a;\na => 5; // ok comment => is here\nb => 5; // this is => 2nd comment\nc => 78"
+puts "===================================\ninput\n==================================="
+puts "#{users}"
+puts "===================================\noutput\n==================================="
+datas = users.split("\n")
+i = 0
+while i < datas.length
+  local_arr = []
+  datas_me = datas[i].split("//")
+  local_arr = datas_me
+  r = local_arr[1] ||= " "
+  result = " "
+  if r != " "
+    result = "//#{local_arr[1]}"
+  end
+  puts "#{local_arr[0].gsub(/=>/, ".")}#{result}"
+  i = i + 1
+end
